0610e6816e82103b6d822c89c55246c1b123051d
Kateryna Troyan <ket.tro@gmail.com>
2025-05-03 16:45:58 +0300

    cookies section

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -5,18 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const COOKIES_KEY = 'cookies-consent';
 
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
   if (localStorage.getItem(COOKIES_KEY)) {
     cookiesBanner.style.display = 'none';
-    document.body.style.overflow = 'auto';
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.overflow = '';
+    window.scrollTo(0, scrollTop);
     return;
   }
 
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollTop}px`;
   document.body.style.overflow = 'hidden';
+  document.body.style.width = '100%';
 
   function handleCookiesResponse(choice) {
     localStorage.setItem(COOKIES_KEY, choice);
     cookiesBanner.style.display = 'none';
-    document.body.style.overflow = 'auto';
+
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.overflow = '';
+    document.body.style.width = '';
+    window.scrollTo(0, scrollTop);
   }
 
   acceptBtn.addEventListener('click', () => handleCookiesResponse('accepted'));
