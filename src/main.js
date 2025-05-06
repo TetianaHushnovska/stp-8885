8eb7655e807d3cc8bc41c6e3a2f9625d4b9a6fb1
tetiana_hushnovska <thushnyovska@gmail.com>
2025-05-06 17:23:33 +0100

    add animation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import './js/burgerMenu';
 import './scss/styles.scss';
 import './js/cookies';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import initSwiper from './js/atmosphere';
 import initFaq from './js/faq';
 
 initSwiper();
 initFaq();
+AOS.init();
