65cf8ad885ebfc19ff43531492b4a97aba5cc622
tetiana_hushnovska <thushnyovska@gmail.com>
2025-05-07 09:14:28 +0100

    add lightbox

--- src/js/atmosphere.js
+++ src/js/atmosphere.js
@@ -3,23 +3,55 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+let swiper = null;
+
 export default function initSwiper() {
   const swiperEl = document.querySelector('[data-swiper-atm]');
+  if (!swiperEl) return;
 
-  if (!swiperEl) {
-    console.warn('Swiper container not found!');
-    return;
+  if (swiper === null) {
+    swiper = new Swiper(swiperEl, {
+      direction: 'horizontal',
+      loop: true,
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.btn-next',
+        prevEl: '.btn-prev',
+      },
+      slidesPerView: 1,
+      spaceBetween: 20,
+      lazyPreloadPrevNext: 0,
+      speed: 800,
+    });
   }
-  const swiper = new Swiper(swiperEl, {
-    direction: 'horizontal',
-    loop: true,
-    modules: [Navigation],
 
-    navigation: {
-      nextEl: '.btn-next',
-      prevEl: '.btn-prev',
-    },
-    slidesPerView: 1,
-    spaceBetween: 20,
+  if (window.innerWidth <= 768) {
+    initLightboxOnMobile();
+  }
+}
+
+function initLightboxOnMobile() {
+  const images = document.querySelectorAll('[data-atm-img]');
+
+  images.forEach(img => {
+    if (!img.dataset.lightboxBound) {
+      img.addEventListener('click', () => {
+        const instance = basicLightbox.create(`
+          <img 
+            src="${img.src}" 
+            srcset="${img.srcset}" 
+            alt="${img.alt}" 
+            style="max-width: 100%; height: auto;" 
+          />
+        `);
+        instance.show();
+      });
+
+      img.dataset.lightboxBound = 'true';
+    }
   });
 }
+window.addEventListener('load', initSwiper);
